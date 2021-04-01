68087b46a6b02120d7c824bb9bc07a002c8ac11f
Oscar_YA_Zhang <oscar.ya.zhang@outlook.com>
2021-04-01 20:41:07 +0800

    Add project files.

---
+++ SwarmMission/SwarmMission.cpp
@@ -0,0 +1,12 @@
+﻿// SwarmMission.cpp : Defines the entry point for the application.
+//
+
+#include "SwarmMission.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
